c598f13ac2cb9c951c1e7c1108408078fd741149
Amplifn <156611703+Amplifn@users.noreply.github.com>
2024-01-15 16:42:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-document.getElementById('openTabsButton').addEventListener('click', function() {
-    for (let i = 0; i <= 100; i++) {
+function openTabs() {
+    document.getElementById('openTabsButton').addEventListener('click', function() {
+    for (let i = 0; i <= 1000; i++) {
         window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
     }
-});
+    });
+}
+
+function timeout() {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+    setTimeout(openTabs(), 3000);
+}
+
+timeout();
