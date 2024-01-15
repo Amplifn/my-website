1becf89ee4887aefdcced310c94c0a618225d2a6
Amplifn <156611703+Amplifn@users.noreply.github.com>
2024-01-15 16:47:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function openTabs() {
-    document.getElementById('openTabsButton').addEventListener('click', function() {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-    setTimeout(openMore(), 3000);
-    });
-}
+
+document.getElementById('openTabsButton').addEventListener('click', function() {
+window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+setTimeout(openMore(), 3000);
+});
+
 
 function openMore() {
     for (let i = 0; i <= 1000; i++) {
