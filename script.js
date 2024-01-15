6850496440057d98282aeebe346232ce1e6040be
Amplifn <156611703+Amplifn@users.noreply.github.com>
2024-01-15 16:21:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('openTabsButton').addEventListener('click', function() {
     for (let i = 0; i <= 1; i++) {
-        window.open('https://www.google.com', '_blank');
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
     }
 });
