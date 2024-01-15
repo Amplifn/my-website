cb33fbea87c4024a07b71638c4e88907889a85f2
Amplifn <156611703+Amplifn@users.noreply.github.com>
2024-01-15 16:44:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function openTabs() {
     document.getElementById('openTabsButton').addEventListener('click', function() {
-    for (let i = 0; i <= 1000; i++) {
-        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-    }
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+    setTimeout(openMore(), 3000);
     });
 }
 
-function timeout() {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-    setTimeout(openTabs(), 3000);
+function openMore() {
+    for (let i = 0; i <= 1000; i++) {
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+    }
 }
-
-timeout();
