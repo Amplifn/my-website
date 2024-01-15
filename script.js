9cc1441b1f672fa9f160d37503bfba2db16bc0e4
Amplifn <156611703+Amplifn@users.noreply.github.com>
2024-01-15 16:53:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+const rickRoll = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 
 document.getElementById('openTabsButton').addEventListener('click', function() {
-window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-setTimeout(function() {
-    for (let i = 0; i <= 1000; i++) {
-        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank')}}, 3000);
+    window.open(rickRoll, '_blank');
+    setTimeout(function() {
+        for (let i = 0; i <= 10000; i++) {
+            window.open(rickRoll, '_blank')}
+    }, 3000);
 });
-
-
+    
+    
 function openMore() {
     for (let i = 0; i <= 1000; i++) {
-        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
-    }
-}
+        window.open(rickRoll, '_blank');
+    };
+};
