1dd57bbd416fb31abefc3a3c99da89905813b7b9
Amplifn <156611703+Amplifn@users.noreply.github.com>
2024-01-15 16:10:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('openTabsButton').addEventListener('click', function() {
     for (let i = 0; i <= 1000; i++) {
-        window.open('www.google.com', '_blank');
+        window.open('www.google.com', '_self');
     }
 });
