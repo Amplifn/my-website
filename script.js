e1348e234b31970097b91c1901a051b7fb39b7d3
Amplifn <156611703+Amplifn@users.noreply.github.com>
2024-01-17 08:29:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const rickRoll = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
 const hugoWebsite = "http://hugo.volny.sk";
 const exampleWeb = "http://example.com";
 
-document.getElementById('openTabsButton').addEventListener('click', function() {
+const openTabsButton = document.getElementById('openTabsButton')
+openTabsButton.addEventListener('click', function() {
     window.open(rickRoll, '_blank');
     setTimeout(function() {openTabs(1000)}, 2000);
 });
